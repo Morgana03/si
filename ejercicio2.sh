959284fd5bbdb222ff48dbfe610206e9688c4bf8
Morgana03 <morganamoralessagasta@gmail.com>
2016-10-05 21:11:58 +0200

    Add files via upload

---
+++ ejercicio2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# != 2 ]
+then
+	echo "ERROR!! Introduzca dos directorios" 
+	exit 1
+fi
+
+if [ ! -d "$1" ]
+then
+	echo "ERROR!! $1 no es un directorio"
+	exit 1
+fi
+
+if [ ! -d "$2" ]
+then
+	echo "ERROR!! $2 no es un directorio"
+	exit 1
+fi
+
+cp -rf "$1" "$2"
+
